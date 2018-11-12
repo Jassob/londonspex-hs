1547c4703b3303077a53f9ae3282dea9c65d9e6b
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-11-12 11:57:50 +0100

    Style LoginForm

--- web/src/LoginForm.js
+++ web/src/LoginForm.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
+import './root.css';
+import './LoginForm.css';
 import Utils from './Utils.js';
 
 class LoginForm extends Component {
@@ -22,60 +28,102 @@ class LoginForm extends Component {
     }
 
     render() {
-	let errItems = this.state.errors.map((err, k) => <li>{err}</li>);
+	let errItems = this.state.errors.map((err, k) => <Grid item className="error" xs={8}>{err}</Grid>);
 	let content = this.state.showRegister ? this.renderRegister() : this.renderLogin();
 	return (
-	    <div>
-	      <ul>{errItems}</ul>
+            <>
+	      <Grid container spacing={8}>
+	        {errItems}
+	      </Grid>
 	      {content}
-	    </div>
+            </>
 	);
     }
 
     renderLogin() {
 	return (
-	    <div>
-	      <h3>Logga in</h3>
-	      <label>E-postadress:</label>
-	      <input name="email"
-		     type="text"
-		     onChange={(event) => this.handleFormUpdate(event)}>
-	      </input> <br />
-	      <label>Lösenord:</label>
-	      <input name="password" type="password"
-		     onChange={(event) => this.handleFormUpdate(event)}>
-	      </input> <br />
-	      <button onClick={() => this.checkLogin()}>Logga in</button> <br />
-	      <label>Har du inget konto?</label>
-	      <button onClick={() => this.setState({showRegister: true})}>Registrera</button>
-	    </div>
+            <form validate>
+              <br/>
+              <Grid container justify="center">
+                <Grid item xs={6}><Typography variant="h5" component="h3">Välkommen, var god logga in</Typography></Grid>
+              </Grid>
+              <Grid container justify="space-around" alignItems="center">
+                <Grid item xs={8} md={4}>
+                  <TextField id="email" type="text" required label="E-postaddress"
+	                     onChange={(event) => this.handleFormUpdate(event)} />
+                </Grid>
+                <Grid item xs={8} md={4}>
+                  <TextField id="password" type="password" required label="Lösendord"
+                             onChange={(event) => this.handleFormUpdate(event)} />
+                </Grid>
+                <Grid item xs={8} md={4}>
+                  <Button color="primary" variant="contained" type="button"
+                          className="center" onClick={() => this.checkLogin()}>Logga in</Button>
+                </Grid>
+              </Grid>
+              <br/>
+              <Grid container spacing={8} justify="space-between">
+	        <Grid item xs={6}><Typography component="p" variant="body2">Har du inget konto?</Typography></Grid>
+	        <Grid item xs={4}>
+                  <Button size="small" variant="outlined" onClick={() => this.setState({showRegister: true})}>Registrera</Button>
+                </Grid>
+              </Grid>
+	    </form>
 	);
     }
 
     renderRegister() {
 	return (
-	    <div>
-	      <h3>Registrera dig</h3>
-	      <label>Namn:</label>
-	      <input name="name"
-		     type="text"
-		     onChange={(event) => this.handleFormUpdate(event)}>
-	      </input> <br />
-	      <label>Email:</label>
-	      <input name="email"
-		     type="email"
-		     onChange={(event) => this.handleFormUpdate(event)}>
-	      </input> <br />
-	      <label>Lösenord:</label>
-	      <input name="password"
-		     type="password"
-		     onChange={(event) => this.handleFormUpdate(event)}>
-	      </input> <br />
-	      <button onClick={() => this.checkRegister()}>Registrera</button>
-	      <br />
-	      <label>Redan registrerad?</label>
-	      <button onClick={() => this.setState({showRegister: false})}>Logga in</button>
-	    </div>
+            <form>
+              <br/>
+              <Grid container justify="center" alignContent="center" alignItems="baseline" spacing={8}>
+                <Grid item xs={8} md={6}>
+                  <Grid container alignContent="center">
+                    <Grid item xs={12} md={7}>
+                      <Typography variant="h5" component="h3">Välkommen, var god fyll i detaljerna nedan</Typography>
+                    </Grid>
+                    <Grid item xs={8} md={6}>
+	              <TextField id="name" type="text" label="Namn" required
+                                 onChange={(event) => this.handleFormUpdate(event)} />
+	            </Grid>
+                  </Grid>
+                  <Grid container>
+	            <Grid item xs={8} md={10}>
+                      <TextField id="email" type="email" label="E-postaddress" required
+                                 onChange={(event) => this.handleFormUpdate(event)} />
+                    </Grid>
+	          </Grid>
+                  <Grid container spacing={8}>
+	            <Grid item xs={8} md={10}>
+                      <TextField name="password" type="password" label="Lösenord" required
+		             onChange={(event) => this.handleFormUpdate(event)} />
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={16}>
+	            <Grid item xs={12}>
+                      <Button color="primary" variant="contained" onClick={() => this.checkRegister()}>
+                        Registrera
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item xs={8} md={5}>
+                  <Typography variant="body2" component="p">Genom att skapa ett konto godkänner du att datan lagras så länge spexet är i London och max en vecka till. Datan du lämnar kommer endast att användas för att du ska kunna logga in på sidan.</Typography>
+                </Grid>
+              </Grid>
+              <br />
+              <hr/>
+              <Grid container spacing={8} justify="space-around">
+                <Grid item>
+                  <Typography components="p" variant="body2">Redan registrerad?</Typography>
+                </Grid>
+                <Grid item>
+	          <Button size="small" variant="outlined"
+                          onClick={() => this.setState({showRegister: false})}>Logga in
+                  </Button>
+                </Grid>
+              </Grid>
+	    </form>
 	);
     }
 
